058d56855b528feff333e0c1d428c82165d340b3
Sebastian Fekete <aight8@gmail.com>
2017-05-15 09:51:58 +0200

    Test

--- server.js
+++ server.js
@@ -3,5 +3,6 @@ const express = require('express')
 const app = express()
 
 app.use('/', express.static('build/prod'));
+app.use('/', express.static('public'));
 
 app.listen(process.env.PORT || 7070)
